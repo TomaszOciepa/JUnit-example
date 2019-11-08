ca623e45fe42d64541dc177365c288b29033b70e
tomaszociepa <tom.ociepa@gmail.com>
2019-11-08 10:34:56 +0100

    unit test - short version

--- src/test/java/calculator/MathTest.java
+++ src/test/java/calculator/MathTest.java
@@ -9,14 +9,22 @@ public class MathTest {
     //BDD
     @Test
    public void should_add_two_numbers() {
+//        //given
+//        Math math = new Math();
+//
+//        //when
+//        int result = math.add(1, 5);
+//
+//        //then
+//        Assert.assertEquals(result, 6);
+
         //given
         Math math = new Math();
 
-        //when
-        int result = math.add(1, 5);
-
         //then
-        Assert.assertEquals(result, 6);
+        Assert.assertEquals(math.add(1,5), 6);
+        Assert.assertEquals(math.add(-1,5), 4);
+        Assert.assertEquals(math.add(10,5), 15);
 
     }
 }
