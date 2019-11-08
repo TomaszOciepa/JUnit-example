cc4c0b1684fd0758b4f0fb8236d5e6fdd9d64b78
tomaszociepa <tom.ociepa@gmail.com>
2019-11-08 11:11:25 +0100

    unit test divide method

--- src/main/java/calculator/Math.java
+++ src/main/java/calculator/Math.java
@@ -5,4 +5,12 @@ public class Math {
     public int add(int a, int b){
         return a + b;
     }
+
+    public double divide(double number1, double number2){
+        if (number2 == 0){
+            throw new ArithmeticException("number can not by divide by 0");
+        }
+
+        return number1/number2;
+    }
 }
--- src/test/java/calculator/MathTest.java
+++ src/test/java/calculator/MathTest.java
@@ -24,7 +24,34 @@ public class MathTest {
         //then
         Assert.assertEquals(math.add(1,5), 6);
         Assert.assertEquals(math.add(-1,5), 4);
-        Assert.assertEquals(math.add(10,5), 15);
+        Assert.assertEquals(math.add(2,-2), 0);
 
+    }
+
+    @Test
+    public void should_no_add_two_numbers() {
+        //given
+        Math math = new Math();
+
+        //then
+        Assert.assertNotEquals(math.add(2,5), 6);
+    }
+
+    @Test
+    public void should_divide_two_numbers() {
+        //given
+        Math math = new Math();
+
+        //then
+        Assert.assertEquals(math.divide(4.03,2), 2.015, 0);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void should_no_divide_by_zero() {
+        //given
+        Math math = new Math();
+
+        //then
+        Assert.assertEquals(math.divide(100,0), 10, 0);
     }
 }
